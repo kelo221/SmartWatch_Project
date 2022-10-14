9710a33db00fa20f9a919e8f9835afc6c3c20b53
kelo221 <61495413+kelo221@users.noreply.github.com>
2022-10-14 20:17:51 +0300

    Event creation content

--- frontend/src/Components/CreateEventsPage.tsx
+++ frontend/src/Components/CreateEventsPage.tsx
@@ -1,33 +1,28 @@
-import { Card, DateInput, Form, FormField, MaskedInput } from "grommet";
-import React, { useState } from "react";
+import { Box, Button, Card, CheckBox, DateInput, Form, FormField, MaskedInput } from "grommet";
+import React from "react";
 
-const maskTime = [
+
+const IPv4ElementExp = /^[0-1][0-9][0-9]$|^2[0-4][0-9]$|^25[0-5]$|^[0-9][0-9]$|^[0-9]$/;
+
+const maskIp = [
   {
-    placeholder: "hh",
     length: [1, 2],
-    options: Array.from({ length: 12 }, (key, val) => val + 1),
-    regexp: /^1[0,1-2]$|^0?[1-9]$|^0$/
+    regexp: IPv4ElementExp,
+    placeholder: "xx"
   },
   { fixed: ":" },
   {
-    placeholder: "mm",
-    length: 2,
-    options: ["00", "15", "30", "45"],
-    regexp: /^[0-5][0-9]$|^[0-9]$/
-  },
-  { fixed: " " },
-  {
-    placeholder: "ap",
-    length: 2,
-    options: ["am", "pm"],
-    regexp: /^[ap]m$|^[AP]M$|^[aApP]$/
+    length: [1, 2],
+    regexp: IPv4ElementExp,
+    placeholder: "xx"
   }
 ];
 
+const ipValidation = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/;
 
 const CreateEventsPage = () => {
 
-  const [time, setTime] = useState("");
+  const [checked, setChecked] = React.useState(false);
 
   return (
     <Card
@@ -38,22 +33,40 @@ const CreateEventsPage = () => {
       justify="center"
       fill
     >
-      <DateInput
-        format="mm/dd/yyyy"
-        value={(new Date()).toISOString()}
-        onChange={({ value }) => {
-        }}
-      />
       <Form>
-        <FormField label="Time" htmlFor="masked-time" name="maskedTime">
+        <FormField
+          label="Event Time"
+          name="maskedIpAddressValidation"
+          htmlFor="masked-ip-address-validation"
+          validate={{
+            regexp: ipValidation,
+            message: "Invalid IP address"
+          }}
+        >
           <MaskedInput
-            name="maskedTime"
-            id="masked-time"
-            mask={maskTime}
-            value={time}
-            onChange={event => setTime(event.target.value)}
+            name="maskedIpAddressValidation"
+            id="masked-ip-address-validation"
+            mask={maskIp}
           />
         </FormField>
+
+        <DateInput
+          format="mm/dd/yyyy"
+          value={(new Date()).toISOString()}
+          onChange={({ value }) => {
+          }}
+        />
+
+        <CheckBox
+          checked={checked}
+          label="Silent?"
+          onChange={(event) => setChecked(event.target.checked)}
+        />
+
+        <Box direction="row" gap="small" pad={"small"}>
+          <Button type="submit" label="Send to Smartwatch" primary />
+          <Button type="reset" label="Reset" />
+        </Box>
       </Form>
     </Card>
   );
