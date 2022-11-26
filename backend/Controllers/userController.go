eb27ff02743ea6fa571937d2e2174bd58b3d2e52
kelo221 <villekum@metropolia.fi>
2022-11-26 20:36:44 +0200

    Rename Project, Remove submodule

--- backend/Controllers/userController.go
+++ backend/Controllers/userController.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	databaseHandler "SmartWatch_Project/db"
 	dbModels "SmartWatch_Project/db/models"
-	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
 	"strconv"
@@ -168,9 +167,6 @@ func DeleteEventDate(c *fiber.Ctx) error {
 	user := c.Locals("user").(*jwt.Token)
 	claims := user.Claims.(jwt.MapClaims)
 	idString := claims["ID"].(float64)
-
-	fmt.Println(c.Request())
-	fmt.Println("-----------")
 
 	var data map[string]string
 
