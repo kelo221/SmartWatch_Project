f7be5a3f51d7663d4006120cb65e441e2e83308e
Takatomo Kodera <64578877+Taka2993@users.noreply.github.com>
2022-12-02 17:02:22 +0200

    Update main.cpp

--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -1,44 +1,90 @@
 #include <Arduino.h>
-#include <iostream>
-#include "API_Handler.hh"
-#include "beeper.hh"
-#include <WiFi.h>
-#include <HTTPClient.h>
 
 
-///TODO CLEAR PASSWORD BEFORE PUSHING
-const char* wifiSSID = "ESP_POINT";
-const char* wifiPassword = "";
-///TODO CLEAR PASSWORD BEFORE PUSHING
+#include "arduinoFFT.h"
+ 
+#define SAMPLES 128             
+#define SAMPLING_FREQUENCY 2048 
+ 
+arduinoFFT FFT = arduinoFFT();
+ 
+unsigned int samplingPeriod;
+unsigned long microSeconds;
+ 
+double vReal[SAMPLES]; 
+double vImag[SAMPLES]; 
+const int buttonPin = digitalRead(4); 
+int count =0;
 
-void setup() {
-    Serial.begin(115200);
-    WiFi.begin(wifiSSID, wifiPassword);
 
-    for(int loopCounter = 0; WiFiClass::status() != WL_CONNECTED; ++loopCounter) {
-        Serial.println("Connecting...");
-        sleep(1);
+void setup() 
+{
+    Serial.begin(115200); 
+    samplingPeriod = round(1000000*(1.0/SAMPLING_FREQUENCY)); 
+      pinMode(buttonPin, INPUT_PULLUP);
+  }
 
-        if (loopCounter==5){
-            Serial.println("Failed to connect.");
-            exit(69);
+ 
+void loop() 
+{  
+    
+    for(int i=0; i<SAMPLES; i++)
+    {
+        microSeconds = micros();    
+     
+        vReal[i] = analogRead(5); 
+        vImag[i] = 0; 
+
+       
+        while(micros() < (microSeconds + samplingPeriod))
+        {
+          
         }
     }
-
-    auto http = new HTTPClient();
-    auto token = getBearerToken(*http,"test","test");
-    auto events = getEvents(*http, token);
-
-    Serial.println(deleteEvent(*http,token,1669458780) ? "OK!!" : "FAILED!!");
-
-    beep();
-
-    for(const eventSpace::event& i : events) {
-        Serial.printf("%s\n", i.get_eventname().c_str());
+     enum standardTuning {
+        E2 = 82,
+        A2  = 110,
+        D3  = 147,
+        G3 = 196,
+        B3 = 250,
+        E4 =  330,
+    };
+    static int button1_last = 0;
+  int button1_state = digitalRead(buttonPin);
+ 
+  while (button1_state != button1_last)
+  {
+    count++;
+    button1_last = button1_state;
+    if (count >= 4) {
+       count = 1;
     }
+  }
 
-}
+  
+     if(count==1)
+        Serial.println(E2);
+        
+     else if(count==2)
+        Serial.println(A2);
+        
+     else if(count==3)
+        Serial.println(D3);
+        
+  
 
-void loop() {
-    sleep(1);
+
+  
+ 
+   
+    FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+    FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+    FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+
+    
+    double peak = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);
+    Serial.println(peak/2);     
+ 
+    
+    delay(2000); 
 }
